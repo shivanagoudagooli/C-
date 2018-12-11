03e156e7a6d250205d2e43abe3b8dd3b03da873b
shivanagoudagooli <43290109+shivanagoudagooli@users.noreply.github.com>
2018-12-11 23:17:43 +0530

    added via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp23
 {
-    class ConsoleApp1 
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ConsoleApp1");
-          Console.ReadKey();           }
+            int i = 11;
+            int j = 44;
+
+            if (i <= 10)
+            {
+                if (i <= 44)
+                {
+                    Console.WriteLine("i value is less than 44:{0}", i);
+
+                }
+                else
+                {
+                    Console.WriteLine("i value is greater than 44:{0}", i);
+                }
+
+            }
+            else
+            {
+                if (j<= 11)
+                {
+                    Console.WriteLine("j value is greater than i  that is j:{0}", j);
+                }
+
+                else
+                {
+                    Console.WriteLine(" j value is less than j and value is j:{0}", j);
+                }
+            }
+
+        }
     }
 }
