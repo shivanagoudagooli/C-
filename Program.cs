09279027541147537e28cc4010f74febf881750c
shivanagoudagooli <43290109+shivanagoudagooli@users.noreply.github.com>
2018-12-11 23:33:48 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "shiva";
+            int age = 20;
+            Console.WriteLine("user name is" +" " + name + "  " +"user age is" + age);
+          
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
