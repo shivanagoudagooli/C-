c775ff227e051f21d27d8a289b49ca095ef11085
shivanagoudagooli <43290109+shivanagoudagooli@users.noreply.github.com>
2018-12-13 00:35:31 +0530

    added via upload

--- Program.cs
+++ Program.cs
@@ -4,43 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp67
+namespace ConsoleApp80
 {
-    interface IUser
+    public class User
     {
-        void GetDetails(string x);
-    }
-
-    class User:IUser
-    {
-        public void GetDetails(string a)
+        public string name = "shiva";
+        public string location = " gouda";
+        public void GetUserDetails()
         {
-            Console.WriteLine("Name:{0}", a);
+            Console.WriteLine("name:{0}", name);
+            Console.WriteLine("location:{0}", location);
         }
     }
 
-     class User1:IUser
-    {
-        public void GetDetails(string a)
-        {
-            Console.WriteLine("location:{0}", a);
-        }
 
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            IUser u = new User();
-            u.GetDetails("shiva");
-
-            User1 u1 = new User1();
-            u1.GetDetails("hadagali");
-            Console.ReadLine();
-
+            User u = new User();
+            u.GetUserDetails();
         }
     }
-
-    
 }
