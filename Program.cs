6c0f7ae20647d72d2986d3eb5e7d10486c05331a
shivanagoudagooli <43290109+shivanagoudagooli@users.noreply.github.com>
2018-12-13 00:27:15 +0530

    added via upload

--- Program.cs
+++ Program.cs
@@ -4,17 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp36
+namespace ConsoleApp47
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string>() { "shiva", "loki" };
-            foreach(string name in names)
-            {
-                Console.WriteLine("name");
-            }
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
